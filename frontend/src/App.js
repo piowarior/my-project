cc11936a29a63c6df2263255aad245f2a25b1e70
piowarior <muhamadrohisuliman@gmail.com>
2025-03-19 13:27:51 +0700

    belajar buat project

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>hallo</h1>
+      <input class="" placeholder='masukkan nama'></input>
+    </div>
+  )
+}
+
+export default App
